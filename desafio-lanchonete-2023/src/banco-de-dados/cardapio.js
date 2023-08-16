67b7a93f8bb1ea816b3a9e3bf5f9f55657c1e3da
Joice Mota <joicemota98@hotmail.com>
2023-08-16 10:57:43 -0300

    Primeira revisao

---
+++ desafio-lanchonete-2023/src/banco-de-dados/cardapio.js
@@ -0,0 +1,10 @@
+export const cardapio = {
+    cafe: { descricao: 'Café', valor: 300 },
+    chantily: { descricao: 'Chantily (extra do Café)', valor: 1.50 },
+    suco: { descricao: 'Suco Natural', valor: 620 },
+    sanduiche: {descricao: 'Sanduíche', valor:650},
+    queijo: {descricao: 'Queijo(extra do Sanduíche', valor:200},
+    salgado: {descricao: 'Salgado', valor:725},
+    combo1: {descricao:'1 Suco e 1 Sanduíche', valor:950},
+    combo2: {descricao:'1 Café e 1 Sanduíche', valor:750}
+}
--- desafio-lanchonete-2023/src/caixa-da-lanchonete.js
+++ desafio-lanchonete-2023/src/caixa-da-lanchonete.js
@@ -1,9 +1,84 @@
-class CaixaDaLanchonete {
+import { cardapio } from "./banco-de-dados/cardapio.js"
 
+class CaixaDaLanchonete {
+    valorTotal = 0
+            
     calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+        if (itens.length == 0) {
+            return "Não há itens no carrinho de compra!"
+        }
+        this.somar(itens)
+      
+        let temChantily = false
+        let temQueijo = false
+        let temCafe = false
+        let temSanduiche = false
+        for (const item of itens) {
+            const [codigo] = item.split(',')
+            
+            // find
+            if (!cardapio[codigo]) {
+                return "Item inválido!"
+            }  
+            if (codigo === 'chantily') {
+                temChantily = true
+            }
+            if (codigo === 'queijo') {
+                temQueijo = true
+            }
+            if (codigo === 'cafe'){
+                temCafe = true
+            }
+            if (codigo === 'sanduiche'){
+                temSanduiche = true
+            }
+        }      
+      
+        // *---------------- Forma de pagamento 
+        if(metodoDePagamento !== 'dinheiro'&& metodoDePagamento !== 'credito' && metodoDePagamento !== 'debito'){
+            return "Forma de pagamento inválida!"
+        }
+        
+        if (metodoDePagamento === 'dinheiro') {
+            if(temChantily && itens.length === 1){
+                return "Item extra não pode ser pedido sem o principal"
+            }
+            const desconto = this.valorTotal * 0.05
+            this.valorTotal -= desconto
+        } else if (metodoDePagamento === 'credito') {
+            if ((temChantily && !temCafe) || (temQueijo && itens.length === 1) ){
+                return "Item extra não pode ser pedido sem o principal"
+            }
+            const acrescimo = this.valorTotal * 0.03
+            this.valorTotal += acrescimo
+        } else{
+            if(temQueijo && !temSanduiche){
+                return "Item extra não pode ser pedido sem o principal"
+            }
+        }
+
+        if(this.valorTotal === 0){
+            return "Quantidade inválida!"
+        }
+        
+        const valorFormatado = (this.valorTotal/100).toFixed(2)
+        return `R$ ${valorFormatado.toString().replace('.',',')}`
+
     }
 
+    somar(itens){
+        // Reduce
+        for (const item of itens) {
+            const [codigo, quantidade] = item.split(',')
+            if (!cardapio[codigo]) {
+                return "Item inválido!"
+            } 
+            const valorItem = cardapio[codigo].valor * parseInt(quantidade)
+            this.valorTotal += valorItem
+        }
+    }
 }
-
+    
+        
 export { CaixaDaLanchonete };
+        
